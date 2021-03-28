52376ac35e54b6c66f628a7b2f92c74cdbec7ce3
tarkravch <tarkravch@gmail.com>
2021-03-28 12:30:31 +0300

    refactoring2

--- src/js/themes.js
+++ src/js/themes.js
@@ -8,20 +8,6 @@ const Theme = {
 
 themeSwitcher.addEventListener('change', newThemeApply);
 
-/* function switchToDarkMode() {
-    themeSwitcher.setAttribute('checked', 'true');
-    themeObject.classList.remove(Theme.LIGHT);
-    themeObject.classList.add(Theme.DARK);
-    localStorage.setItem('theme', JSON.stringify(Theme.DARK));
-};
-
-function switchToLightMode() {
-    themeSwitcher.removeAttribute('checked');
-    themeObject.classList.remove(Theme.DARK);
-    themeObject.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', JSON.stringify(Theme.LIGHT));
-} */
-
 function switchMode(toRemove, toAdd) {
     if (toAdd === Theme.DARK) {
         themeSwitcher.setAttribute('checked', 'true');
